e63925ced05b3d2517eeb7698f00ed567e31c674
Lee0701 <hgl1002@naver.com>
2023-04-05 18:54:11 +0900

    Remove dictionary classes

--- app/src/main/java/io/github/lee0701/mboard/dictionary/Dictionary.kt
+++
@@ -1,6 +0,0 @@
-package io.github.lee0701.mboard.dictionary
-
-interface Dictionary<T> {
-    fun search(key: List<Byte>): T?
-    fun entries(): List<Pair<List<Byte>, T>>
-}
--- app/src/main/java/io/github/lee0701/mboard/dictionary/MutableDictionary.kt
+++
@@ -1,6 +0,0 @@
-package io.github.lee0701.mboard.dictionary
-
-interface MutableDictionary<T>: Dictionary<T> {
-    fun put(key: List<Byte>, value: T)
-    fun remove(key: List<Byte>)
-}
