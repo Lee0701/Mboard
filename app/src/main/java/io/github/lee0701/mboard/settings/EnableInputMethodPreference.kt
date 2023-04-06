a50c06abb988b79ec457ff0a292c23bef9fd5654
Lee0701 <hgl1002@naver.com>
2023-04-07 08:52:51 +0900

    Add input method settings category

---
+++ app/src/main/java/io/github/lee0701/mboard/settings/EnableInputMethodPreference.kt
@@ -0,0 +1,16 @@
+package io.github.lee0701.mboard.settings
+
+import android.content.Context
+import android.content.Intent
+import android.provider.Settings
+import android.util.AttributeSet
+import androidx.preference.Preference
+
+class EnableInputMethodPreference(
+    context: Context,
+    attrs: AttributeSet?,
+): Preference(context, attrs) {
+    override fun onClick() {
+        context.startActivity(Intent(Settings.ACTION_INPUT_METHOD_SETTINGS))
+    }
+}
