f514856c4bbff477975479e43b8b021735903dac
Lee0701 <hgl1002@naver.com>
2023-04-10 15:55:59 +0900

    Use assets instead of raw resources

--- app/src/main/java/io/github/lee0701/mboard/dictionary/DictionaryManager.kt
+++ app/src/main/java/io/github/lee0701/mboard/dictionary/DictionaryManager.kt
@@ -1,27 +1,19 @@
 package io.github.lee0701.mboard.dictionary
 
-import android.annotation.SuppressLint
-import android.content.res.Resources
-import android.content.res.Resources.NotFoundException
+import android.content.res.AssetManager
 import java.io.IOException
 
 object DictionaryManager {
-    fun loadCompoundDictionary(resources: Resources, names: List<String>, packageName: String): CompoundDictionary<HanjaDictionary.Entry> {
+    fun loadCompoundDictionary(assetManager: AssetManager, names: List<String>): CompoundDictionary<HanjaDictionary.Entry> {
         val dictionaries = names.mapNotNull { name ->
-            loadDictionary(resources, name, packageName)
+            loadDictionary(assetManager, name)
         }
         return CompoundDictionary(dictionaries)
     }
-    @SuppressLint("DiscouragedApi")
-    fun loadDictionary(resources: Resources, name: String, packageName: String): ListDictionary<HanjaDictionary.Entry>? {
+    fun loadDictionary(assetManager: AssetManager, name: String): ListDictionary<HanjaDictionary.Entry>? {
         return try {
-            val id = resources.getIdentifier(name, "raw", packageName)
-            if(id > 0) DiskDictionary(resources.openRawResource(id))
-            else null
+            DiskDictionary(assetManager.open("dict/$name.bin"))
         } catch (ex: IOException) {
-            ex.printStackTrace()
-            null
-        } catch(ex: NotFoundException) {
             ex.printStackTrace()
             null
         }
--- app/src/main/java/io/github/lee0701/mboard/module/external/HanjaConverter.kt
+++ app/src/main/java/io/github/lee0701/mboard/module/external/HanjaConverter.kt
@@ -2,7 +2,6 @@ package io.github.lee0701.mboard.module.external
 
 import android.content.Context
 import android.content.pm.PackageManager
-import android.content.res.Resources
 import android.widget.Toast
 import io.github.lee0701.mboard.R
 import io.github.lee0701.mboard.dictionary.DictionaryManager
@@ -15,19 +14,14 @@ object HanjaConverter {
             "io.github.lee0701.converter.donation",
             "io.github.lee0701.converter",
         )
-        var resources: Resources? = null
-        var packageName: String? = null
         for(name in packageNames) {
             try {
-                resources = context.packageManager.getResourcesForApplication(name)
-                packageName = name
+                val packageContext = context.createPackageContext(name, 0) ?: continue
+                return DictionaryManager.loadCompoundDictionary(packageContext.assets, listOf("base"))
             } catch(ex: PackageManager.NameNotFoundException) {
-                Toast.makeText(context, R.string.msg_hanja_converter_not_found, Toast.LENGTH_LONG).show()
             }
-            if(resources != null) break
         }
-        return if(resources != null && packageName != null)
-            DictionaryManager.loadCompoundDictionary(resources, listOf("base"), packageName)
-        else null
+        Toast.makeText(context, R.string.msg_hanja_converter_not_found, Toast.LENGTH_LONG).show()
+        return null
     }
 }
