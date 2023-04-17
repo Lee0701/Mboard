41e6b7abc4e18bc9731aac40fe82bde1a10f7e6e
Lee0701 <hgl1002@naver.com>
2023-04-17 19:08:39 +0900

    Convert combination table entry ints to hex

--- app/src/main/java/io/github/lee0701/mboard/module/table/JamoCombinationTable.kt
+++ app/src/main/java/io/github/lee0701/mboard/module/table/JamoCombinationTable.kt
@@ -1,10 +1,11 @@
 package io.github.lee0701.mboard.module.table
 
+import io.github.lee0701.mboard.module.serialization.HexIntSerializer
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class JamoCombinationTable(
-    val list: List<List<Int>> = listOf(),
+    val list: List<List<@Serializable(with = HexIntSerializer::class) Int>> = listOf(),
 ) {
     val map: Map<Pair<Int, Int>, Int> = list.associate { (a, b, result) -> (a to b) to result }
 
