7da8245e3b7b5dab5ea10bffa3a1418add852df0
Lee0701 <hgl1002@naver.com>
2023-04-29 02:38:50 +0900

    Make candidates view fully work

--- app/src/main/java/io/github/lee0701/mboard/module/component/CandidatesComponent.kt
+++ app/src/main/java/io/github/lee0701/mboard/module/component/CandidatesComponent.kt
@@ -4,12 +4,11 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup.LayoutParams
-import androidx.recyclerview.widget.LinearLayoutManager
 import io.github.lee0701.mboard.databinding.ComponentCandidatesBinding
 import io.github.lee0701.mboard.module.candidates.BasicCandidatesAdapter
 import io.github.lee0701.mboard.module.candidates.Candidate
 import io.github.lee0701.mboard.module.candidates.CandidateListener
-import io.github.lee0701.mboard.module.candidates.DefaultCandidate
+import io.github.lee0701.mboard.settings.TouchInterceptingFrameLayout
 
 class CandidatesComponent(
     private val width: Int,
@@ -25,12 +24,17 @@ class CandidatesComponent(
         val inflater = LayoutInflater.from(context)
         val binding =
             ComponentCandidatesBinding.inflate(inflater)
+
         binding.root.layoutParams = LayoutParams(width, height)
         val adapter = BasicCandidatesAdapter(context) { listener?.onItemClicked(it) }
         binding.recyclerView.adapter = adapter
+
+        val wrapper = TouchInterceptingFrameLayout(context, null, disableTouch)
+        wrapper.addView(binding.root)
+
         this.binding = binding
         this.adapter = adapter
-        return binding.root
+        return wrapper
     }
 
     override fun updateView() {
--- app/src/main/java/io/github/lee0701/mboard/settings/TouchInterceptingFrameLayout.kt
+++ app/src/main/java/io/github/lee0701/mboard/settings/TouchInterceptingFrameLayout.kt
@@ -4,10 +4,17 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.widget.FrameLayout
+import io.github.lee0701.mboard.R
 
 class TouchInterceptingFrameLayout(
     context: Context,
     attrs: AttributeSet?,
 ): FrameLayout(context, attrs) {
-    override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean = true
+    constructor(context: Context, attrs: AttributeSet?, intercept: Boolean): this(context, attrs) {
+        this.intercept = intercept
+    }
+
+    private var intercept: Boolean = false
+
+    override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean = intercept
 }
