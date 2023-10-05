26aff04717c8cb6a424d2d5b8d5a36f141a2d1e2
Lee0701 <hgl1002@naver.com>
2023-10-05 21:23:54 +0900

    Add icons for setting categories

--- app/src/main/java/io/github/lee0701/mboard/settings/preference/Preference.kt
+++ app/src/main/java/io/github/lee0701/mboard/settings/preference/Preference.kt
@@ -2,7 +2,11 @@ package io.github.lee0701.mboard.settings.preference
 
 import android.content.Context
 import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.LinearLayoutCompat
+import androidx.core.view.MarginLayoutParamsCompat
 import androidx.preference.Preference
+import androidx.preference.PreferenceViewHolder
 import io.github.lee0701.mboard.R
 
 class Preference(
@@ -11,5 +15,15 @@ class Preference(
 ): Preference(context, atts) {
     init {
         layoutResource = R.layout.preference_inline
+    }
+
+    override fun onBindViewHolder(holder: PreferenceViewHolder) {
+        super.onBindViewHolder(holder)
+        val icon = holder.findViewById(android.R.id.icon) as AppCompatImageView
+        val layoutParams = icon.layoutParams as LinearLayoutCompat.LayoutParams
+        val margin = if(icon.drawable != null)
+            context.resources.getDimensionPixelSize(R.dimen.pref_icon_margin)
+        else 0
+        MarginLayoutParamsCompat.setMarginEnd(layoutParams, margin)
     }
 }
